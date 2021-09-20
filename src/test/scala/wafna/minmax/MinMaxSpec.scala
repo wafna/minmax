76cd72a61fa832ea9d064b5ca250f446ac575980
Jason Feingold <jfeingold@gmail.com>
2021-09-19 19:31:25 -0700

    test game eval

--- src/test/scala/wafna/minmax/MinMaxSpec.scala
+++ src/test/scala/wafna/minmax/MinMaxSpec.scala
@@ -11,38 +11,40 @@ class MinMaxSpec extends TestBase {
 
   implicit val mm: MinMax[Game] = GameTreeMinMax
   "MinMax" should {
+    def runTree(depth: Int)(expected: String)(tree: Game): Assertion = {
+      expected shouldBe MinMax.search(tree, depth).get.game.id
+    }
     "select the best move" in {
-      def runTree(depth: Int)(expected: String)(tree: Game): Assertion = {
-        expected shouldBe MinMax.search(tree, depth).get.game.id
+      runTree(1)("1-2") {
+        // format: off
+        Game("0", None, P1, List(
+          Game("1-1", None, P2),
+          Game("1-2", Some(1), P2),
+          Game("1-3", Some(-1), P2)
+        ))
+        // format: on
       }
-//      runTree(1)("1-2") {
-//        // format: off
-//        GameTree("0", 0, P1, List(
-//          GameTree("1-1", 0, P2, List()),
-//          GameTree("1-2", 1, P2, List()),
-//          GameTree("1-3", -1, P2, List())
-//        ))
-//        // format: on
-//      }
-//      runTree(1)("1-4") {
-//        // format: off
-//        GameTree("0", 0, P1, List(
-//          GameTree("1-1", 0, P2, List.empty),
-//          GameTree("1-2", 1, P2, List.empty),
-//          GameTree("1-3", -1, P2, List.empty),
-//          GameTree("1-4", 2, P2, List.empty)
-//        ))
-//        // format: on
-//      }
+      runTree(1)("1-4") {
+        // format: off
+        Game("0", None, P1, List(
+          Game("1-1", Some(0), P2),
+          Game("1-2", Some(1), P2),
+          Game("1-3", Some(-1), P2),
+          Game("1-4", Some(2), P2)
+        ))
+        // format: on
+      }
+    }
+    "prune the tree" in {
       runTree(2)("1-1") {
         // format: off
-        Game("1", 0, P1, List(
-          Game("1-1", 0, P2, List(
-            Game("1-1-1", -1, P1, List.empty)
+        Game("1", None, P1, List(
+          Game("1-1", None, P2, List(
+            Game("1-1-1", Some(-1), P1)
           )),
-          Game("1-2", 0, P2, List(
-            Game("1-2-1", -2, P1, List.empty),
-            Game("1-2-2", 1, P1, List.empty)
+          Game("1-2", None, P2, List(
+            Game("1-2-1", Some(-2), P1),
+            Game("do not evaluate!", None, P1)
           ))
         ))
         // format: on
@@ -53,11 +55,13 @@ class MinMaxSpec extends TestBase {
 
 object MinMaxSpec {
 
-  case class Game(id: String, eval: Int, player: Player, moves: List[Game])
+  sealed trait Node
+  case class Game(id: String, eval: Option[Int], player: Player, moves: List[Game] = List.empty) extends Node
 
   implicit object GameTreeMinMax extends MinMax[Game] {
     override def currentPlayer(game: Game): util.Player = game.player
-    override def evaluate(game: Game, player: util.Player): Int = game.eval
+    override def evaluate(game: Game, player: util.Player): Int =
+      game.eval.getOrElse(sys.error(s"Illegal evaluation at game ${game.id}"))
     override def moves(game: Game): Seq[Game] = game.moves.toSeq
     override def pass(game: Game): Game = ???
   }
