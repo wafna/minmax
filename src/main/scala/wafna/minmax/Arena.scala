f6342bb049fea228875d7d6e130ea9b53ce3a816
Jason Feingold <jfeingold@gmail.com>
2021-09-25 21:33:57 -0700

    cyclometric complexity

--- src/main/scala/wafna/minmax/Arena.scala
+++ src/main/scala/wafna/minmax/Arena.scala
@@ -1,7 +1,8 @@
 package wafna.minmax
 
-import wafna.minmax.MinMax.Eval
+import wafna.minmax.MinMax.{Eval, State}
 import wafna.minmax.MinMax.State.{Drawn, Open, Won}
+import wafna.util.Player
 import wafna.util.Player.{P1, P2}
 
 import scala.annotation.tailrec
@@ -54,27 +55,22 @@ object Arena {
 
   def runMatch[G](game: G, p1: Bot[G], p2: Bot[G], games: Int)(implicit minMax: MinMax[G]): Match = {
     require(0 < games)
-    val score = Iterator.continually(game).take(games).foldLeft((0, 0, 0)) { (score, game) =>
-      val score1 = minMax.state(runGame(game, p1, p2)) match {
+    def keepScore(state: State): (Int, Int, Int) = {
+      state match {
         case Open  => ArenaException(s"Open game.")
-        case Drawn => (score._1, score._2, score._3 + 1)
+        case Drawn => (0, 0, 1)
         case Won(winner) =>
           winner match {
-            case P1 => (score._1 + 1, score._2, score._3)
-            case P2 => (score._1, score._2 + 1, score._3)
+            case P1 => (1, 0, 0)
+            case P2 => (0, 1, 0)
           }
 
       }
-      minMax.state(runGame(game, p2, p1)) match {
-        case Open  => ArenaException(s"Drawn game.")
-        case Drawn => (score._1, score._2, score._3 + 1)
-        case Won(winner) =>
-          winner match {
-            case P1 => (score1._1, score1._2 + 1, score1._3)
-            case P2 => (score1._1 + 1, score1._2, score1._3)
-          }
-
-      }
+    }
+    val score = Iterator.continually(game).take(games).foldLeft((0, 0, 0)) { (score, game) =>
+      val score1 = keepScore(minMax.state(runGame(game, p1, p2)))
+      val score2 = keepScore(minMax.state(runGame(game, p2, p1)))
+      (score._1 + score1._1 + score2._2, score._2 + score1._2 + score2._1, score._3 + score1._3 + score2._3)
     }
     Match(p1.show(), p2.show(), score._1, score._2, score._3)
   }
