90da97dc78025082c8a9811739cb659c27a75e3d
wafna <wafna@duck.com>
2021-11-27 20:35:16 -0800

    prettier still

--- src/main/scala/wafna/onimata/Onimata.scala
+++ src/main/scala/wafna/onimata/Onimata.scala
@@ -1,6 +1,5 @@
 package wafna.onimata
 
-import scala.collection.immutable.ArraySeq
 import scala.util.chaining._
 
 case class Hand(c1: Card, c2: Card) {
@@ -8,9 +7,10 @@ case class Hand(c1: Card, c2: Card) {
     if (c1 == out) { Hand(in, c2) }
     else if (c2 == out) { Hand(c1, in) }
     else {
-      throw new IllegalArgumentException(s"Card $c1 not in hand ${c1 -> c2}")
+      throw new IllegalArgumentException(s"Card $c1 not in hand $toList")
     }
   }
+  def toList: List[Card] = List(c1, c2)
 }
 
 sealed trait Player
@@ -31,13 +31,13 @@ class Onimata private (p1: Hand, p2: Hand, pass: Either[Card, Card], board: Boar
     def moveP2(hand: Hand, passCard: Card, board: Board) =
       new Onimata(p1, hand, Right(passCard), board)
 
-    val (player, hand @ Hand(c1, c2), passCard, mover) = pass match {
+    val (player, hand, passCard, mover) = pass match {
       case Right(c) => (P1, p1, c, moveP1 _)
       case Left(c)  => (P2, p2, c, moveP2 _)
     }
 
     val pawns = board.occupied(player)
-    List(c1, c2).flatMap { card =>
+    hand.toList.flatMap { card =>
       val hand1 = hand.cycle(card, passCard)
       val moves = (for {
         pawn <- pawns
