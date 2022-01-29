89f4d66de37cc9b9799d9e9bab37c4121f26c1da
wafna <wafna@duck.com>
2022-01-29 13:41:57 -0800

    test spots

--- src/test/scala/wafna/onimata/SpotTest.scala
+++ src/test/scala/wafna/onimata/SpotTest.scala
@@ -7,8 +7,8 @@ class SpotTest extends TestBase {
   "Spot" should {
     "be valid" in {
       (for {
-        x <- 0 to 4
-        y <- 0 to 4
+        x <- 0 until 5
+        y <- 0 until 5
       } yield (x, y)).foreach { case (x, y) =>
         Spot(x, y).valid shouldBe true
       }
@@ -21,13 +21,22 @@ class SpotTest extends TestBase {
       Spot(5, 4).valid shouldBe false
       Spot(5, 5).valid shouldBe false
     }
-    "index" in {
+    "toIx" in {
       Spot(0, 0).toIx shouldBe 0
       Spot(1, 0).toIx shouldBe 1
       Spot(0, 1).toIx shouldBe 5
       Spot(4, 1).toIx shouldBe 9
       Spot(4, 2).toIx shouldBe 14
       Spot(4, 4).toIx shouldBe 24
+    }
+    "fromIx" in {
+      (for {
+        x <- 0 until 5
+        y <- 0 until 5
+      } yield (x, y)).foreach { case (x, y) =>
+        val spot = Spot(x, y)
+        spot shouldBe Spot.fromIx(spot.toIx)
+      }
     }
     "move" in {
       val origin = Spot(0, 0)
