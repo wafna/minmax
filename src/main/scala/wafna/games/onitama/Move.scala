d52c0f9cd31c4b5ec7ee20692015812ac97e3f08
wafna <wafna@duck.com>
2022-02-02 09:50:54 -0800

    test onitama

--- src/main/scala/wafna/games/onitama/Move.scala
+++ src/main/scala/wafna/games/onitama/Move.scala
@@ -8,23 +8,41 @@ final case class Move(x: Int, y: Int)
 case class Card(name: String, moves: ArraySeq[Move])
 
 object Deck {
+  
+  val Monkey: Card = Card("Monkey", ArraySeq(Move(-1, -1), Move(1, -1), Move(-1, 1), Move(1, 1)))
+  val Dragon: Card = Card("Dragon", ArraySeq(Move(-1, -1), Move(1, -1), Move(-2, 1), Move(2, 1)))
+  val Tiger: Card = Card("Tiger", ArraySeq(Move(0, 2), Move(0, -1)))
+  val Ox: Card = Card("Ox", ArraySeq(Move(1, 0), Move(0, -1), Move(0, 1)))
+  val Horse: Card = Card("Horse", ArraySeq(Move(-1, 0), Move(0, -1), Move(0, 1)))
+  val Goose: Card = Card("Goose", ArraySeq(Move(-1, 0), Move(-1, 1), Move(1, 0), Move(1, -1)))
+  val Rooster: Card = Card("Rooster", ArraySeq(Move(-1, 0), Move(-1, -1), Move(1, 0), Move(1, 1)))
+  val Cobra: Card = Card("Cobra", ArraySeq(Move(-1, 0), Move(1, 1), Move(1, -1)))
+  val Eel: Card = Card("Eel", ArraySeq(Move(1, 0), Move(-1, 1), Move(-1, -1)))
+  val Mantis: Card = Card("Mantis", ArraySeq(Move(-1, 1), Move(1, 1), Move(0, -1)))
+  val Crane: Card = Card("Crane", ArraySeq(Move(-1, -1), Move(1, -1), Move(0, 1)))
+  val Rabbit: Card = Card("Rabbit", ArraySeq(Move(-1, -1), Move(1, 1), Move(2, 0)))
+  val Frog: Card = Card("Frog", ArraySeq(Move(1, -1), Move(-1, 1), Move(-2, 0)))
+  val Boar: Card = Card("Boar", ArraySeq(Move(-1, 0), Move(1, 0), Move(0, 1)))
+  val Crab: Card = Card("Crab", ArraySeq(Move(-2, 0), Move(0, 1), Move(2, 0)))
+  val Elephant: Card = Card("Elephant", ArraySeq(Move(-1, 0), Move(-1, 1), Move(1, 0), Move(1, 1)))
+
   val cards: ArraySeq[Card] = ArraySeq(
-    Card("Monkey", ArraySeq(Move(-1, -1), Move(1, -1), Move(-1, 1), Move(1, 1))),
-    Card("Dragon", ArraySeq(Move(-1, -1), Move(1, -1), Move(-2, 1), Move(2, 1))),
-    Card("Tiger", ArraySeq(Move(0, 2), Move(0, -1))),
-    Card("Ox", ArraySeq(Move(1, 0), Move(0, -1), Move(0, 1))),
-    Card("Horse", ArraySeq(Move(-1, 0), Move(0, -1), Move(0, 1))),
-    Card("Goose", ArraySeq(Move(-1, 0), Move(-1, 1), Move(1, 0), Move(1, -1))),
-    Card("Rooster", ArraySeq(Move(-1, 0), Move(-1, -1), Move(1, 0), Move(1, 1))),
-    Card("Cobra", ArraySeq(Move(-1, 0), Move(1, 1), Move(1, -1))),
-    Card("Eel", ArraySeq(Move(1, 0), Move(-1, 1), Move(-1, -1))),
-    Card("Mantis", ArraySeq(Move(-1, 1), Move(1, 1), Move(0, -1))),
-    Card("Crane", ArraySeq(Move(-1, -1), Move(1, -1), Move(0, 1))),
-    Card("Rabbit", ArraySeq(Move(-1, -1), Move(1, 1), Move(2, 0))),
-    Card("Frog", ArraySeq(Move(1, -1), Move(-1, 1), Move(-2, 0))),
-    Card("Boar", ArraySeq(Move(-1, 0), Move(1, 0), Move(0, 1))),
-    Card("Crab", ArraySeq(Move(-2, 0), Move(0, 1), Move(2, 0))),
-    Card("Elephant", ArraySeq(Move(-1, 0), Move(-1, 1), Move(1, 0), Move(1, 1)))
+    Monkey,
+    Dragon,
+    Tiger,
+    Ox,
+    Horse,
+    Goose,
+    Rooster,
+    Cobra,
+    Eel,
+    Mantis,
+    Crane,
+    Rabbit,
+    Frog,
+    Boar,
+    Crab,
+    Elephant
   )
   def apply()(implicit random: Random = Random): (Hand, Hand, Card) = {
     val deck = random.shuffle(cards)
--- src/test/scala/wafna/games/onitama/OnitamaTest.scala
+++ src/test/scala/wafna/games/onitama/OnitamaTest.scala
@@ -1,11 +1,33 @@
 package wafna.games.onitama
 
 import wafna.TestBase
+import wafna.games.onitama.Deck._
 
 class OnitamaTest extends TestBase {
+
+  implicit class HandContainsCard(hand: Hand) {
+    def contains(card: Card): Boolean = hand match {
+      case Hand(`card`, _) => true
+      case Hand(_, `card`) => true
+      case _ => false
+    }
+  }
+
   "Onitama" should {
     "Calculate moves" in {
-      val g0 = Onitama()
+      val g0 = Onitama(Hand(Monkey, Dragon),Hand(Tiger, Ox), Horse)
+      val moves = g0.moves()
+      // 4 pieces can use monkey to go up left
+      // 4 pieces can use monkey to go up right
+      // 3 for dragon up left and 3 for dragon up right
+      assertResult(14)(moves.size)
+      // P2 has turn in hand.
+      assert(moves.forall(_.pass.isLeft))
+      // Each card in hand used as expected.
+      assertResult(8)(moves.count(_.pass == Left(Monkey)))
+      assertResult(6)(moves.count(_.pass == Left(Dragon)))
+      // Pass card now in hand.
+      assertResult(14)(moves.count(_.p1.contains(Horse)))
     }
   }
 }
