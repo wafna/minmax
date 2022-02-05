b2f03a0cf3d20cf4591f90f3c00efe42233e485e
wafna <wafna@duck.com>
2022-02-05 10:11:02 -0800

    draw disallowed

--- src/main/scala/wafna/games/onitama/OnitamaMinMax.scala
+++ src/main/scala/wafna/games/onitama/OnitamaMinMax.scala
@@ -16,7 +16,9 @@ object OnitamaMinMax {
     override def currentPlayer(game: Onitama): Player = if (game.pass.isRight) P1 else P2
 
     override def evaluate(game: Onitama, player: Player): Int = game.gameOver match {
-      case Some(Draw) => 0 // why not?
+      case Some(Draw) =>
+        // Even if the player cannot move a piece the player must still exchange a card.
+        sys.error("Draw disallowed.")
       case Some(Win(p)) => if (p == player) Int.MaxValue else Int.MinValue
       case None =>
         // todo worth favoring some early game optimizations?
@@ -34,10 +36,16 @@ object OnitamaMinMax {
 
   def main(args: Array[String]): Unit = {
     implicit val random: Random = scala.util.Random
-    val (result, games): (GameOver, List[Onitama]) = Arena.runGame(Onitama(), new RandomBot[Onitama](), new SearchBot[Onitama](2))
+    val (result, games): (GameOver, List[Onitama]) = Arena.runGame(Onitama(), new SearchBot[Onitama](4), new SearchBot[Onitama](4))
     println(result)
-    games.foreach { game =>
+    val turns = games.size
+    games.zipWithIndex.foreach { case (game, turn) =>
+      println(s"-- Turn ${turns - turn}")
       println(Console.show(game).mkString("\n"))
     }
+    val g = games.head
+    val cards: List[Card] = g.p1.toNel.toList ++ g.p2.toNel.toList ++ List(g.pass.getOrElse(g.pass.swap.toOption.get))
+    println("----------------------")
+    cards.foreach(card => println(Console.show(card).mkString("\n")))
   }
 }
