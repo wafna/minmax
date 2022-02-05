aaa8689277f9cefad6d602b84228137f6738c20c
wafna <wafna@duck.com>
2022-02-05 13:36:21 -0800

    minmax listener data

--- src/main/scala/wafna/games/minmax/MinMax.scala
+++ src/main/scala/wafna/games/minmax/MinMax.scala
@@ -25,19 +25,23 @@ case class Win(player: Player) extends GameOver
 object MinMax {
 
   class MinMaxError(msg: String) extends Exception(msg)
+
   object MinMaxError {
     def apply(msg: String): Nothing = throw new MinMaxError(msg)
   }
 
   trait Listener {
-    def search(): Unit
+
+    def search(depth: Int): Unit
     def prune(): Unit
-    def evaluate(): Unit
+    def evaluate(eval: Int): Unit
   }
+
   object ListenerNoOp extends Listener {
-    override def search(): Unit = ()
+
+    override def search(depth: Int): Unit = ()
     override def prune(): Unit = ()
-    override def evaluate(): Unit = ()
+    override def evaluate(eval: Int): Unit = ()
   }
 
   case class Stats(searches: Int, prunes: Int, evaluations: Int)
@@ -46,83 +50,88 @@ object MinMax {
     private var searches = 0
     private var prunes = 0
     private var evaluations = 0
-    override def search(): Unit = searches += 1
+
+    override def search(depth: Int): Unit = searches += 1
     override def prune(): Unit = prunes += 1
-    override def evaluate(): Unit = evaluations += 1
+    override def evaluate(eval: Int): Unit = evaluations += 1
+
     def stats(): Stats = Stats(searches, prunes, evaluations)
   }
 
   /** A game plus its valuation relative to the searching player. */
   final case class Eval[G](game: G, eval: Int)
 
-  @inline private def selectBest[G](mm: Int, best: Option[Eval[G]], maybe: Eval[G]): Option[Eval[G]] =
-    if (best.exists(mm * _.eval > mm * maybe.eval)) {
-      best
-    } else {
-      Some(maybe)
-    }
-
   type Evaluator[G] = (G, Player) => Int
 
   /** Search the current game to the specified depth for the best move.
     */
+  //noinspection ScalaStyle
   def search[G](game: G, maxDepth: Int, evaluator: Evaluator[G])(implicit
-    minMax: MinMax[G], listener: Listener = ListenerNoOp
+    minMax: MinMax[G],
+    listener: Listener = ListenerNoOp
   ): Either[GameOver, Eval[G]] = {
     require(0 < maxDepth, "maxDepth must be positive.")
     // This player is the player initiating the search, throughout.
     val searchingPlayer = minMax.currentPlayer(game)
+
+    @inline def selectBest[G](mm: Int, best: Option[Eval[G]], maybe: Eval[G]): Option[Eval[G]] =
+      if (best.exists(mm * _.eval > mm * maybe.eval)) {
+        best
+      } else {
+        Some(maybe)
+      }
+
+    /** Below the first layer of search we may have a pruning value which we use to abort unfruitful searches.
+      */
+    def searchPruned[G](game: G, evaluator: Evaluator[G], prune: Option[Int], depth: Int)(implicit
+      minMax: MinMax[G],
+      listener: Listener
+    ): Int = {
+
+      if (0 == depth) {
+        val eval = evaluator(game, searchingPlayer)
+        listener.evaluate(eval)
+        eval
+      } else {
+        listener.search(maxDepth - depth)
+        // This flips the sense of inequalities used in finding best moves and pruning searches.
+        val mm = if (minMax.currentPlayer(game) == searchingPlayer) 1 else -1
+
+        @tailrec
+        def searchMoves(moves: Seq[G], best: Option[Eval[G]]): Int = moves match {
+          case Nil =>
+            // We should have selected a best move by now.
+            best.map(_.eval).getOrElse(MinMaxError(s"No moves!"))
+          case m :: ms =>
+            val eval = searchPruned(m, evaluator, prune = best.map(_.eval), depth = depth - 1)
+            if (prune.exists(p => mm * p < mm * eval)) {
+              listener.prune()
+              eval
+            } else {
+              val b = selectBest(mm, best, Eval(m, eval))
+              searchMoves(ms, b)
+            }
+        }
+
+        minMax.moves(game) match {
+          case Left(Draw)        => 0
+          case Left(Win(winner)) => if (searchingPlayer == winner) Int.MaxValue else Int.MinValue
+          case Right(moves) =>
+            searchMoves(moves.toList, None)
+        }
+      }
+    }
+
     minMax
       .moves(game)
       .map {
         _.foldLeft(Option.empty[Eval[G]]) { (best, move) =>
           // prune with the current best value.
-          val eval: Int = searchPruned(move, searchingPlayer, evaluator, prune = best.map(_.eval), depth = maxDepth - 1)
+          val eval: Int = searchPruned(move, evaluator, prune = best.map(_.eval), depth = maxDepth - 1)
           // always maximizing at the top.
           selectBest(1, best, Eval(move, eval))
         }.getOrElse(MinMaxError(s"No moves!"))
       }
-  }
 
-  /** Below the first layer of search we may have a pruning value which we use to abort unfruitful searches.
-    */
-  private def searchPruned[G](
-    game: G,
-    searchingPlayer: Player,
-    evaluator: Evaluator[G],
-    prune: Option[Int],
-    depth: Int
-  )(implicit minMax: MinMax[G], listener: Listener): Int = {
-    if (0 == depth) {
-      listener.evaluate()
-      evaluator(game, searchingPlayer)
-    } else {
-      listener.search()
-      // This flips the sense of inequalities used in finding best moves and pruning searches.
-      val mm = if (minMax.currentPlayer(game) == searchingPlayer) 1 else -1
-
-      @tailrec
-      def searchMoves(moves: Seq[G], best: Option[Eval[G]]): Int = moves match {
-        case Nil =>
-          // We should have selected a best move by now.
-          best.map(_.eval).getOrElse(MinMaxError(s"No moves!"))
-        case m :: ms =>
-          val eval = searchPruned(m, searchingPlayer, evaluator, prune = best.map(_.eval), depth = depth - 1)
-          if (prune.exists(p => mm * p < mm * eval)) {
-            listener.prune()
-            eval
-          } else {
-            val b = selectBest(mm, best, Eval(m, eval))
-            searchMoves(ms, b)
-          }
-      }
-
-      minMax.moves(game) match {
-        case Left(Draw)        => 0
-        case Left(Win(winner)) => if (searchingPlayer == winner) Int.MaxValue else Int.MinValue
-        case Right(moves) =>
-          searchMoves(moves.toList, None)
-      }
-    }
   }
 }
